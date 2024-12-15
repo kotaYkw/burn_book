64b737a5468d90a774041bb1a93a01a944fe09c5
kotaYkw <yukawa.kota.yj@alumni.tsukuba.ac.jp>
2024-12-15 23:44:17 +0900

    🎉 init commit

---
+++ my_burn_app/src/main.rs
@@ -0,0 +1,12 @@
+use burn::tensor::{Tensor, backend::Backend};
+
+fn computation<B: Backend>() {
+    let devise = Default::default();
+    let tensor1: Tensor<B, 2> = Tensor::from_floats([[2., 3.], [4., 5.]], &devise);
+    let tensor2 = Tensor::ones_like(&tensor1);
+    println!("{:}", tensor1 + tensor2);
+}
+
+fn main() {
+    computation::<burn::backend::Wgpu>();
+}
